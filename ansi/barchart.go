de69d4d15f1cbbb8e6bde96d649b4f090ba1302f
Ömür Özkir <oemuer.oezkir@gmail.com>
2017-04-12 13:01:31 +0200

    draws a basic box, wip: barchart

--- ansi/barchart.go
+++ ansi/barchart.go
@@ -3,6 +3,23 @@ package ansi
 import ui "github.com/gizak/termui"
 
 func Draw() error {
-	err := ui.Init()
-	return err
+	if err := ui.Init(); err != nil {
+		return err
+	}
+	defer ui.Close()
+	bc := createBarChart()
+	ui.Render(bc)
+	ui.Handle("/sys/kbd/q", func(ui.Event) {
+		ui.StopLoop()
+	})
+	ui.Loop()
+	return nil
+}
+
+func createBarChart() *ui.BarChart {
+	bc := ui.NewBarChart()
+	bc.Width = 50
+	bc.Height = 30
+	bc.BorderLabel = "score(total)"
+	return bc
 }
